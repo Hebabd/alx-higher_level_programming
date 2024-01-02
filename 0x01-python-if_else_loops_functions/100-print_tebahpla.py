d833d09433580fc8cf7bfcf533dfbd34d558ba61
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-02 08:18:01 -0800

    G

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    c = i + ord('A')
-    if i % 2 == 1:
-        c +- 32
-        print("{:c}".format(c), end="")
+for i in range(122, 96, -1):
+    
+    if i % 2 == 0:
+        n = chr(i)
+    else:
+        n = chr(i-32)
+        print("{}".format(n), end="")
