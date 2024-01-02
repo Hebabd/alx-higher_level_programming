13c9d1042ddf2963badd88831a381191e36cc75a
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-02 12:20:10 -0800

    G

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-    i = 122
-    while i >= 97:
-        flag = 0
-        if i % 2 != 0:
-            i = i - 32
-            flag = 1
-            print("{:s}".format(chr(i)), end="")
-            if flg == 1:
-                i = i + 32
-                i = i - 1
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flg == 1:
+        i = i + 32
+    i = i - 1
 
-                print()
+print()
