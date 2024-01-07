cdc103ef6837a3b79b9ae8cae495643c1c671166
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-07 03:02:17 -0800

    11

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,22 +4,23 @@
  * print_python_list_info - prints basic info about python lists.
  * @p: A Pyobject list
  */
-void print_python_list_info(pyObject *p)
+void print_python_list_info(py0bject *p)
 {
 	int size, alloc, i;
-	pyObject *obj;
+	py0bject *obj;
 
 	size = py_SIZE(p);
-	alloc = ((pyListObjet *)p)->allocated;
+	alloc = ((pyList0bjet *)p)->allocated;
 
 	printf("[*] Size of the python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element &d: ", i);
+		printf("Element %d: ", i);
 
 		obj = pyList_GetItem(p, i);
 		printf("%s\n", py_TYPE(obj)->tp_name);
 	}
 }
+
