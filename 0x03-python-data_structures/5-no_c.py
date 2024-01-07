2472adcf1449173e2b878e116e54ea77ca6c288b
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-07 01:26:05 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    str = ""
-    for i in my_string[:]:
-        if i != 'c' and i != 'c':
-            str += i
-    return (str)
+    tmp_string = list(my_string)
+    for c in range(0, len(tmp_string)):
+        if ord(tmp_string[c]) == ord('c') or ord(tmp_string[c]) == ord('c'):
+            tmp_string[c] = ''
+    return ''.join(tmp_string)
