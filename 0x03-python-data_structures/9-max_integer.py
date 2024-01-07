327bf598329e9489d65081122c7df59980cfa8f6
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-07 01:49:46 -0800

    9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    m = 0
-    for i in my_list:
+    m = my_list[0]
+    for i in my_list[-1]:
         if i > m:
             m = i
     return m
