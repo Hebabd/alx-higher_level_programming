9b8225d6b7243522b5efdb1851332924b82b71b5
Hebabd <hebatallahabdelhamid90@gmail.com>
2024-01-07 02:16:08 -0800

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     c = tuple_b[0] if len(tuple_b) > 0 else 0
     d = tuple_b[1] if len(tuple_b) > 1 else 0
     new_tuple = (a + c, b + d)
-    return(new_tuple)
+    return new_tuple
